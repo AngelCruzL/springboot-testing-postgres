ffb7a8f32647a3be97799317e8189e0b2f9717e7
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-04 10:59:32 -0600

    First commit

---
+++ src/main/java/dev/angelcruzl/SpringbootTestingPsqlApplication.java
@@ -0,0 +1,13 @@
+package dev.angelcruzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestingPsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestingPsqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/angelcruzl/SpringbootTestingPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.angelcruzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingPsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
